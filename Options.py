9496dcfa741ef6ecca716ddbbabd227f3c58dc46
Hyeongwoo Moon <moc9058@gmail.com>
2022-12-25 17:46:12 +0900

    added and edited some files

--- Options.py
+++ Options.py
@@ -2,14 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Option():
-    def __init__(self, K=60, position='long', value_range=100):
+    def __init__(self, K, position, value_range):
         self.exercise = K
         self.value_range = value_range
         assert position.lower() in ['long','short'], "You're in either long or short position."
         self.position = position.lower()
         
 class CallOption(Option):
-    def __init__(self, K=60, position='long', value_range=100):
+    def __init__(self, K, position, value_range=100):
         super().__init__(K=K, position=position, value_range=value_range) 
         
     def payoff(self):
@@ -24,7 +24,7 @@ class CallOption(Option):
         return xx
 
 class PutOption(Option):
-    def __init__(self, K=60, position='long', value_range=100):
+    def __init__(self, K, position, value_range=100):
         super().__init__(K=K, position=position, value_range=value_range) 
     
     def payoff(self):
@@ -39,7 +39,7 @@ class PutOption(Option):
         return xx
 
 class Asset():
-    def __init__(self, position='long', value_range=100):
+    def __init__(self, position, value_range=100):
         assert position.lower() in ['long','short'], "You're in either long or short position."
         self.position = position.lower()
         self.value_range = value_range
@@ -52,7 +52,7 @@ class Asset():
             return -xx
 
 class Bond():
-    def __init__(self,K=40, position='long', value_range=100):
+    def __init__(self,K, position, value_range=100):
         self.debt = K
         assert position.lower() in ['long','short'], "You're in either long or short position."
         self.position = position.lower()
@@ -71,7 +71,8 @@ call_40_long = CallOption(K=40, position='long')
 bond_40_long = Bond(K=40, position='long')
 xx = np.linspace(0,100,101)
 
-plt.title("(A) Payoff at option expiration")
+plt.figure(figsize=(11,9))
+plt.title("(A) Payoff at option expiration", fontdict={'size':22})
 plt.gca().spines['top'].set_visible(False)
 plt.gca().spines['right'].set_visible(False)
 plt.gca().spines['bottom'].set_position(('data',0))
@@ -83,12 +84,33 @@ plt.plot(xx, call_40_long.payoff()+40.3, 'k')
 plt.xlim([0,100])
 plt.ylim([-10,80])
 plt.xticks([])
-plt.yticks([0])
+plt.yticks([])
 
-plt.text(40, -7, "K", fontdict={'size':15})
-plt.text(-5, 40, "K", fontdict={'size':15})
+plt.text(40, -4, "K", fontdict={'size':20})
+plt.text(-4, 40, "K", fontdict={'size':20})
 plt.savefig("Strategy (A).png")
-plt.show()
 
 
 # Strategy (B)
+put_40_long = PutOption(K=40, position='long')
+asset_long = Asset(position='long')
+xx = np.linspace(0,100,101)
+
+plt.figure(figsize=(11,9))
+plt.title("(B) Payoff at option expiration", fontdict={'size':22})
+plt.gca().spines['top'].set_visible(False)
+plt.gca().spines['right'].set_visible(False)
+plt.gca().spines['bottom'].set_position(('data',0))
+
+plt.plot(xx, put_40_long.payoff()+0.2, 'r--')
+plt.plot(xx, asset_long.payoff(), 'b--')
+plt.plot(xx, call_40_long.payoff()+40.3, 'k')
+
+plt.xlim([0,100])
+plt.ylim([-10,80])
+plt.xticks([])
+plt.yticks([])
+
+plt.text(40, -4, "K", fontdict={'size':20})
+plt.text(-4, 40, "K", fontdict={'size':20})
+plt.savefig("Strategy (B).png")
